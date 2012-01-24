aae8efc567485ea6a5a0b6b969bbf79233157069
Peter McGrath <pete.j.mcgrath@gmail.com>
2012-01-23 19:42:20 -0500

    Added some more styes to handle the presentation of the deal and discard piles. There is an error with the stock to discard pile in everygame after the initial game. Overall the js is getting messy. A major reorg and clean up is needed.

---
+++ _games/klondike/js/aura/permissions.js
@@ -0,0 +1,56 @@
+define([], function () {
+
+	// Permissions
+
+	// A permissions structure can support checking
+	// against subscriptions prior to allowing them 
+	// to clear. This enforces a flexible security 
+	// layer for your application.
+
+	var permissions = {
+
+		newContentAvailable: {
+			contentUpdater:true
+		},
+
+		
+		emptyStock:{
+			todoSaver:true
+		}
+/*
+		beginContentEditing:{
+			editFocus:true
+		},
+
+		addingNewTodo:{
+			todoTooltip:true
+		},
+
+		clearContent:{
+			garbageCollector:true
+		},
+
+		renderDone:{
+			todoCounter:true //switch to false to see what happens :)
+		},
+
+		destroyContent:{
+			todoRemover:true
+		},
+
+		createWhenEntered:{
+			keyboardManager:true
+		}
+*/
+
+	};
+
+	permissions.validate = function(subscriber, channel){
+		var test = permissions[channel][subscriber];
+		return test===undefined? false: test;
+	};
+
+
+	return permissions;
+
+});
